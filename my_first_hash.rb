fdd53afd042758bcfc633a8ef21db4b6b307eb79
DAZUN XING <dazun.xing@gmail.com>
2020-04-02 09:53:21 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-hashme={"name"=>"daz","weight"=>"180", "hobby"=>"guitar"}
+hashme={"name"=>"daz","weight"=>180, "hobby"=>"guitar"}
   
 end
 
@@ -9,7 +9,7 @@ def shipping_manifest
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information: 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
-
+the_manifest={"whale bone corsets"=>5, "procelain vases"=>2, "oil paintings"=>3}
 
 end
 
